f45b85460f4971e21e901747209b33e29b127d3c
Spyder-code <luaysyauqillah@gmail.com>
2023-10-26 15:35:35 +0700

    update create message

--- app/Livewire/MessageInvitation.php
+++ app/Livewire/MessageInvitation.php
@@ -9,7 +9,12 @@ use Illuminate\Support\Str;
 
 class MessageInvitation extends Component
 {
-    public $data, $is_created, $code, $name, $weeding, $message, $invitation;
+    public $data, $is_created, $code, $name, $weeding, $message, $invitation, $no_name;
+
+    protected $rules = [
+        'no_name' => 'required',
+        'message' => 'required',
+    ];
 
     public function mount($code = null, $name = null)
     {
@@ -17,16 +22,10 @@ class MessageInvitation extends Component
         $this->weeding = Weeding::where('code',$code)->first();
         $this->data = WeedingInvitation::where('weeding_id',$this->weeding->id)->whereNotNull('message')->orderBy('updated_at','desc')->get();
         $this->is_created = false;
-        $slug = Str::slug($name);
+        $this->name = $name;
+        $slug = Str::slug(strtolower($name));
         $this->invitation = WeedingInvitation::where('weeding_id',$this->weeding->id)->where('slug',$slug)->first();
-        if(!$this->invitation){
-            $this->invitation = WeedingInvitation::create([
-                'weeding_id' => $this->weeding->id,
-                'slug' => $slug,
-                'name' => str_replace('-',' ',$slug)
-            ]);
-        }
-        $this->message = $this->invitation->message;
+        $this->message = $this->invitation->message ?? '';
     }
 
     public function render()
@@ -37,15 +36,32 @@ class MessageInvitation extends Component
     public function write()
     {
         $this->is_created = !$this->is_created;
+        $this->data = WeedingInvitation::where('weeding_id',$this->weeding->id)->whereNotNull('message')->orderBy('updated_at','desc')->get();
     }
 
     public function send()
     {
-        $this->invitation->update([
-            'message' => $this->message
-        ]);
+        if(!$this->invitation){
+            if($this->no_name){
+                $slug = Str::slug(strtolower($this->no_name));
+                $this->invitation = WeedingInvitation::create([
+                    'weeding_id' => $this->weeding->id,
+                    'slug' => $slug,
+                    'name' => $this->no_name,
+                    'message' => $this->message
+                ]);
+                $this->is_created = false;
+            }else{
+                session()->flash('no_name', 'wajib diisi!');
+                $this->is_created = true;
+            }
+        }else{
+            $this->invitation->update([
+                'message' => $this->message
+            ]);
+            $this->is_created = false;
+        }
 
         $this->data = WeedingInvitation::where('weeding_id',$this->weeding->id)->whereNotNull('message')->orderBy('updated_at','desc')->get();
-        $this->is_created = false;
     }
 }
--- resources/views/livewire/message-invitation.blade.php
+++ resources/views/livewire/message-invitation.blade.php
@@ -3,7 +3,11 @@
         <div class="col-12 col-md-4"></div>
         <div class="col-12 col-lg-8" style="border-right: #f4e2be 1px solid;">
             @if ($is_created)
-            <textarea wire:model="message" class="avander p-3" autofocus cols="30" rows="10" style="background: transparent; color: #f4e2be; border: 1px #f4e2be dashed;list-style-type: none; letter-spacing:2px; font-weight:bold"></textarea>
+            @if (is_null($name))
+            @if (session()->has('no_name')) <span class="avander" style="color: #f4e2be; letter-spacing:2px;"><b>Nama harus diisi</b></span> @endif
+            <input type="text" placeholder="Nama" autofocus class="avander p-3" wire:model="no_name" style="background: transparent; color: #f4e2be; border: 1px #f4e2be dashed;list-style-type: none; letter-spacing:2px; font-weight:bold; width:100%; margin-bottom:5px">
+            @endif
+            <textarea wire:model="message" class="avander p-3" {{ is_null($name) ? '' : 'autofocus' }} cols="30" rows="10" style="background: transparent; color: #f4e2be; border: 1px #f4e2be dashed;list-style-type: none; letter-spacing:2px; font-weight:bold; width:100%" placeholder="Ucapan"></textarea>
             <div class="d-flex gap-3">
                 <div wire:click="send" class="animate__animated animate__infinite animate__slow animate__pulse avander ls box px-5 py-3 w-100" style="color: #cbf4be;"><b>Kirim</b></div>
                 <div wire:click="write" class="animate__animated animate__infinite animate__slow animate__pulse avander ls box px-5 py-3 w-100" style="color: #f4c4be;"><b>Batal</b></div>
